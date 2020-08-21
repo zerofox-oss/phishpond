8d158ccf3fdc4886fd2266041537d096bd4f004e
sysgoblin <cbaylissmk2@gmail.com>
2020-08-21 20:40:06 +0100

    Added helper function cos yea

--- get_shell.sh
+++ get_shell.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
+Help()
+{
+   # Display Help
+   echo "Specify the container you want a shell for ya dingus."
+   echo "Example: ./get_shell.sh webserver"
+}
+
+while getopts ":h" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+   esac
+done
 docker exec -t -i `docker ps | grep "$1" | cut -d " " -f 1` bash
