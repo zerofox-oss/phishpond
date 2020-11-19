17142de80276e1895e9c7e070761eaadf54efb4b
zmallen <zma4580@gmail.com>
2020-11-19 13:51:57 -0500

    Added set default stream context for patch.php. Tested & ready to go!

--- configs/php/patch.php
+++ configs/php/patch.php
@@ -1,7 +1,16 @@
 <?php
+    // Set default stream context if none specified
+    $default_opts = array(
+      'http'=>array(
+	'proxy'=>'mitmproxy:8080'
+      )
+    );
+    $default = stream_context_set_default($default_opts);
+
+    // Hijack stream_context_create in case someone overrides our default
     uopz_set_return('stream_context_create', function(array $options) {
         $array = func_get_args();
-        $array[0]["http"]['proxy']='mitmproxy:8080';
+        $array[0]['http']['proxy']='mitmproxy:8080';
         return stream_context_create($array[0]);
     }, 1);
 ?>
--- configs/php/unpatch.php
+++ configs/php/unpatch.php
@@ -1,4 +1,2 @@
 <?php
-    echo '<br>#####unpatch.php';
-    echo '<br>#####end unpatch.php';
 ?>
