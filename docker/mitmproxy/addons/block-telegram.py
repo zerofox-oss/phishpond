41511d8e3ffb72608991b8e89b255fffc46f6b5b
Zack Allen <zma4580@gmail.com>
2021-04-03 16:11:09 -0400

    Add logging in Telegram plugin for mitmproxy (#24)

--- docker/mitmproxy/addons/block-telegram.py
+++ docker/mitmproxy/addons/block-telegram.py
@@ -7,12 +7,13 @@ Usage:
 
     mitmproxy -s block-telegram.py
 """
-from mitmproxy import ctx, http
-
+from mitmproxy import ctx, http, log
+from mitmproxy.addons import termlog
+import json
 
 class BlockTelegram:
     def __init__(self):
-        self.num = 0
+        self.outfile = '/home/mitmproxy/logs/telegram.log'
 
     def load(self, loader):
         loader.add_option(
@@ -25,9 +26,17 @@ class BlockTelegram:
     def response(self, flow):
         telegram_api_domain = 'api.telegram.org'
         if flow.request.pretty_host.endswith(telegram_api_domain):
+            data = {}
+            data['payload'] = flow.request.get_text()
+            data['headers'] = {key: value for (key, value) in flow.request.headers.items()}
+            data['url'] = flow.request.pretty_url
+            with open(self.outfile, 'a') as fd:
+                json.dump(data, fd)
+                fd.write('\n')
+
             flow.response = http.HTTPResponse.make(
                 201,
-                b"Logged Telegram Data!",
+                b"Logged data to logs/telegram.log",
                 {"Content-Type": "text/html"}
             )
 
